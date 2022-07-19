d9a377b7f9a65e7d800d86e8bb9f61589942e0a1
oluwajueda <oluwajuedaloade@gmail.com>
2022-07-19 13:28:52 -0700

    node work added

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+// Modules
+const names = require('./4-names')
+const sayHi = require('./utils')
+const data = require('./6-alternative-flavor')
+console.log(data)
+require('./7-mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
